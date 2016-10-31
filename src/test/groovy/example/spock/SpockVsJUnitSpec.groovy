9f43c3f1a4fac565f6d868d2869e5a591f34bbfa
RIP21 <puha212@gmail.com>
2016-10-31 15:05:59 +0100

    - SpringBoot App added

---
+++ src/test/groovy/example/spock/SpockVsJUnitSpec.groovy
@@ -0,0 +1,10 @@
+package example.spock
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class SpockVsJUnitSpec extends Specification {
+
+
+}
